79500edacf06927f5969fe1df8800a1c23584967
ranamohs <108424060+ranamohs@users.noreply.github.com>
2024-02-10 02:23:53 +0200

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Box , Stack , Typography}  from '@mui/material';
+import Logo from '../assets/images/Logo-1.png'
+
+
+const Footer = () => {
+  return (
+    <>
+    <Box mt="120px" bgcolor="#fff3f4" pt="50px">
+      <Stack gap="40px" alignItems='center' px="24px">
+        <img src={Logo} alt="logo" width="200px" height="40px" ></img>
+        <Typography variant="h6" pb="40px" mt="20px">
+        Made with ❤️ by rana mohsen
+        </Typography>
+
+      </Stack>
+
+    </Box>
+    </>
+  )
+}
+
+export default Footer
