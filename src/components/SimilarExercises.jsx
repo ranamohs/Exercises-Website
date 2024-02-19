01237aab99194066ed88da078b796604b195a5f5
rana <ranamohsen868_sd@fcis.bsu.edu.eg>
2024-02-19 19:47:06 +0200

    change 1

---
+++ src/components/SimilarExercises.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import HorizontalScrollbar from "./HorizontalScrollbar";
+import Loader from "./Loader";
+
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
+  return (
+    <>
+      <Box sx={{ mt: { lg: "100px", xs: "0" } }}>
+        <Typography variant="h3" mb={8}>
+          Exercises That target the same{" "}
+          <span style={{ color: "#ff2625", textTransform: "capitalize" }}>
+            Muscle
+          </span>{" "}
+          miscle group
+        </Typography>
+        <Stack direction="row" sx={{ p: "2", position: "relative" }}>
+          {targetMuscleExercises.length ? (
+            <HorizontalScrollbar data={targetMuscleExercises} />
+          ) : (
+            <Loader></Loader>
+          )}
+        </Stack>
+
+        <Typography variant="h3" mb={8} mt={15}>
+          Exercises That target the same{" "}
+          <span style={{ color: "#ff2625", textTransform: "capitalize" }}>
+            equipments
+          </span>
+        </Typography>
+        <Stack direction="row" sx={{ p: "2", position: "relative" }}>
+          {equipmentExercises.length ? (
+            <HorizontalScrollbar data={equipmentExercises} />
+          ) : (
+            <Loader></Loader>
+          )}
+        </Stack>
+      </Box>
+    </>
+  );
+};
+
+export default SimilarExercises;
+
+/* <Typography variant="h4" mb="33px">
+Watch{" "}
+<span style={{ color: "#ff2625", textTransform: "capitalize" }}>
+  {name}
+</span>{" "}
+exercise videos
+</Typography> */
---
+++ src/pages/ExerciseDetail.jsx
@@ -0,0 +1,171 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  exerciseOptions,
+  fetchData,
+  youtubeOptions,
+} from "../Utils/fetchExData";
+import Detail from "./../components/Detail";
+import ExerciseVedios from "./../components/ExerciseVedios";
+import SimilarExercises from "./../components/SimilarExercises";
+
+const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
+  const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+  const [equipmentExercises, setEquipmentExercises] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
+      setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`, youtubeOptions);
+      setExerciseVideos(exerciseVideosData.contents);
+
+      const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
+      setTargetMuscleExercises(targetMuscleExercisesData);
+
+      const equimentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions);
+      setEquipmentExercises(equimentExercisesData);
+    };
+
+    fetchExercisesData();
+  }, [id]);
+
+  if (!exerciseDetail) return <div>No Data</div>;
+
+  return (
+    <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVedios exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
+      <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
+    </Box>
+  );
+};
+
+export default ExerciseDetail;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import { Box } from "@mui/material";
+// import {
+//   exerciseOptions,
+//   fetchData,
+//   youtubeOptions,
+// } from "../Utils/fetchExData";
+// import Detail from "./../components/Detail";
+// import ExerciseVedios from "./../components/ExerciseVedios";
+// import SimilarExercises from "./../components/SimilarExercises";
+
+// const ExerciseDetail = () => {
+//   const [exerciseDetail, setExerciseDetail] = useState({});
+//   const [exerciseVideos, setExerciseVideos] = useState([]);
+//   const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+//   const [equipmentExercises, setEquipmentExercises] = useState([]);
+//   const { id } = useParams();
+
+//   useEffect(() => {
+//     const fetchExercisesData = async () => {
+//       const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+//       const youtubeSearchUrl =
+//         "https://youtube-search-and-download.p.rapidapi.com";
+
+//       const exerciseDetailData = await fetchData(
+//         `${exerciseDbUrl}/exercises/exercise/${id}`,
+//         exerciseOptions
+//       );
+//       setExerciseDetail(exerciseDetailData);
+
+//       const exerciseVideosData = await fetchData(
+//         `${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`,
+//         youtubeOptions
+//       );
+//       setExerciseVideos(exerciseVideosData.contents);
+
+//       const targetMuscleExercisesData = await fetchData(
+//         `${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`,
+//         exerciseOptions
+//       );
+//       setTargetMuscleExercises(targetMuscleExercisesData);
+
+//       const equipmentExercisesData = await fetchData(
+//         `${exerciseDbUrl}/exercises/equipment/target/${exerciseDetailData.equipment}`,
+//         exerciseOptions
+//       );
+//       setEquipmentExercises(equipmentExercisesData);
+//     };
+//     fetchExercisesData();
+//   }, [id]);
+
+//   return (
+//     <>
+//       <Box>
+//         <Detail exerciseDetail={exerciseDetail} />
+//         <ExerciseVedios
+//           exerciseVideos={exerciseVideos}
+//           name={exerciseDetail.name}
+//         ></ExerciseVedios>
+//         <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises}></SimilarExercises>
+//       </Box>
+//     </>
+//   );
+// };
+
+// export default ExerciseDetail;
